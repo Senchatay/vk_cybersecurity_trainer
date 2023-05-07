407b4ed60efe6a6e7078f267e92cd14275e1d772
Arseniy Latyshev <alatyshev@phoenixit.ru>
2023-05-07 18:50:26 +0300

    category_1: Добавлена сущность категории

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  belongs_to :category
+  validates :name, presence: true, uniqueness: true
+  validates :category, presence: true
+
+  def category_name
+    category&.name
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_140621) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_145328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_tasks_on_category_id"
+  end
+
+  add_foreign_key "tasks", "categories"
 end
