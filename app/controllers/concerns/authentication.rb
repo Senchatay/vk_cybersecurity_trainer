1838142ab3233c3ebed82ba7426e2433190dc703
Arseniy Latyshev <alatyshev@phoenixit.ru>
2023-05-07 22:48:58 +0300

    users_2: Добавил авторизацию в системе

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Authentication
+  extend ActiveSupport::Concern
+
+  included do
+    private
+
+    def current_user
+      User.find_by(id: session[:user_id]) if session[:user_id].present?
+    end
+
+    def user_signed_in?
+      current_user.present?
+    end
+
+    def sign_in(user)
+      session[:user_id] = user.id
+    end
+
+    def sign_out
+      session.delete :user_id
+      @current_user = nil
+    end
+
+    helper_method :current_user, :user_signed_in?
+  end
+end
